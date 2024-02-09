4dab7d166fd7c334d8216941f8de961d58bc959c
mmarquesEE <mateus.marques@ee.ufcg.edu.br>
2024-02-09 18:27:23 -0300

    updated functions

--- Model Datasheet/model_cycle_life.m
+++ Model Datasheet/model_cycle_life.m
@@ -1,11 +1,11 @@
-clc ,close all
+clear all,clc ,close all
 
 data = csvread("Model Datasheet\Data\cycle_life.csv");
 
 k = data(:, 1);
 Cc0 = data(:, 2); 
 
-f = @(n, a, k) a(1)*exp(-n*k/a(2)) + a(3);
+f = @(a, k) a(1)*exp(-k/a(2)) + a(3);
 
 Ne = 2;
 Cc = Cc0;
@@ -14,22 +14,23 @@ a = zeros(Ne,3);
 
 rng(200);
 for i = 1:Ne
-    a0 = [Cc(1), 500, 0]; 
-    a(i,:) = lsqcurvefit(@(a,k) f(i,a,k), a0, k, Cc);
+    a0 = [Cc(1), 500/i, 0]; 
+    a(i,:) = lsqcurvefit(f, a0, k, Cc);
         
-    Cc = Cc - f(i,a(i,:),k);
-    CcOut = CcOut + f(i,a(i,:),k);
+    Cc = Cc - f(a(i,:),k);
+    CcOut = CcOut + f(a(i,:),k);
 end
 %%
-
-display(['f = ',num2str(a(1,1),2), ...
-    '*exp(-k/',num2str(a(1,2),2),') + ',num2str(a(2,1),2), ...
-    '*exp(-k/',num2str(a(2,2),2),') + ',num2str(sum(a(:,3)),2)]);
+np = 3;
+display(['f = ',num2str(a(1,1),np), ...
+    '*exp(-k/',num2str(a(1,2),np),') + ',num2str(a(2,1),np), ...
+    '*exp(-k/',num2str(a(2,2),np),') + ',num2str(sum(a(:,3)),np)]);
 
 %%
 figure(1);
 plot(k, Cc0, 'bo', 'DisplayName', 'Dados Originais');
 hold on;
+%%
 plot(k, CcOut, 'g-', 'LineWidth', 2, 'DisplayName', 'Modelo Exponencial');
 xlabel('Ciclos de Vida');
 ylabel('Capacidade');
