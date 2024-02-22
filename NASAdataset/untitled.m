fb318c4c0f0a525d26a789d30ec8a2b419e2cc8b
mmarquesEE <mateus.marques@ee.ufcg.edu.br>
2024-02-22 15:41:12 -0300

    added dataStructs

--- NASAdataset/untitled.m
+++ NASAdataset/untitled.m
@@ -1,9 +1,34 @@
-close all,clear all
+close all,clear all,clc
 
-files = strip(string(ls("archive\cleaned_dataset\data\")));
+dataDir = "archive\cleaned_dataset\data\";
+
+files = strip(string(ls(dataDir)));
 files = files(3:end);
 
-a = readtable("archive\cleaned_dataset\data\" + files(2));
-b = readtable("archive\cleaned_dataset\metadata.csv");
+metadata = readtable("archive\cleaned_dataset\metadata.csv");
 
-c = b(string(b.type)=="impedance" & string(b.battery_id) == "B0047",:)
+%% Organize files
+
+% by batteries
+batt_ids = unique(string(metadata.battery_id));
+batt = cell(length(batt_ids),1);
+
+for i = 1:length(batt_ids)
+    batt{i}.id = string(batt_ids(i));
+
+    crr_table = metadata(string(metadata.battery_id) == batt{i}.id,...
+        metadata.Properties.VariableNames);
+
+    discharge = crr_table(string(crr_table.type)=="discharge", ...
+        {'start_time','ambient_temperature','filename','Capacity'});
+    charge = crr_table(string(crr_table.type)=="charge", ...
+        {'start_time','ambient_temperature','filename'});
+    impedance = crr_table(string(crr_table.type)=="impedance", ...
+        {'start_time','ambient_temperature','filename','Re','Rct'});
+
+    batt{i}.discharge = discharge;
+    batt{i}.charge = charge;
+    batt{i}.impedance = impedance;
+end
+
+save("dataStructs.mat","batt");
